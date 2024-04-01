4c9be71bb79ec56a2801713e084ed5a6de96085f
Akbar Sharofiddinov <akbarsharofiddinov@gmail.com>
2024-04-01 15:07:40 +0500

    debug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,7 @@
-import { useEdisonContext } from "@/context/EdisonContext";
+import React from "react";
 
-function Footer() {
-  const { openCart, totalSumm } = useEdisonContext();
-
-  return (
-    <footer className="footer" id="footer">
-      <div className="container">
-        <div className="footer-inner">
-          <div className="totalPrice">
-            <h1>Общая сумма</h1>
-            <p style={{ display: "flex", gap: 5 }}>
-              {totalSumm}
-              <span>сум</span>
-            </p>
-          </div>
-          <button className="order-btn" onClick={openCart}>
-            Заказать
-          </button>
-        </div>
-      </div>
-    </footer>
-  );
-}
+const Footer: React.FC = () => {
+  return <></>;
+};
 
 export default Footer;
